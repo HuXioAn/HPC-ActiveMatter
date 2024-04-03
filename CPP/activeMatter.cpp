a060559585ae11c8c0662cf7eb9aa66387d133a7
Huxioan <hu7006@outlook.com>
2024-04-03 13:45:47 +0200

    [cpp] compute

--- CPP/activeMatter.cpp
+++ CPP/activeMatter.cpp
@@ -8,6 +8,7 @@ Create Time: 02/04/24
 #include <iostream>
 #include <cmath>
 #include <random>
+#include <cstring>
 
 using namespace std;
 
@@ -34,6 +35,10 @@ typedef struct activePara_s
 
 using arrayPtr = shared_ptr<float>;
 
+//0-1 float random
+mt19937 radomGen;
+uniform_real_distribution<float> radomDist;
+
 void computeActiveMatter(generalPara_t gPara, activePara_t aPara, arrayPtr posX, arrayPtr posY, arrayPtr theta);
 
 int main(int argc, char* argv[]){
@@ -59,15 +64,15 @@ int main(int argc, char* argv[]){
         .velocity = 1.0
     };
 
-    //initialize the data
-    srand(gPara.randomSeed);
+    radomGen = mt19937(gPara.randomSeed);
+    radomDist = uniform_real_distribution<float>(0,1);
 
+    //initialize the data
     arrayPtr posX(new float(gPara.birdNum));
     arrayPtr posY(new float(gPara.birdNum));
     arrayPtr theta(new float(gPara.birdNum));
 
-    mt19937 radomGen(gPara.randomSeed);
-    uniform_real_distribution<float> radomDist(0,1);
+
     for(int i=0; i < gPara.birdNum; i++){
         //randomize the pos and theta
         auto randomFloat = radomDist(radomGen);
@@ -91,7 +96,40 @@ int main(int argc, char* argv[]){
 
 void computeActiveMatter(generalPara_t gPara, activePara_t aPara, arrayPtr posX, arrayPtr posY, arrayPtr theta){
 
+    arrayPtr tempTheta(new float(gPara.birdNum));
 
+    for(int step=0; step < gPara.totalStep; step++){ //steps
+
+        for(int bird=0; bird < gPara.birdNum; bird++){ //move
+            //move
+            posX[bird] += gPara.deltaTime * cos(theta[bird]);
+            posY[bird] += gPara.deltaTime * sin(theta[bird]);
+
+            //in the field
+            posX[bird] = fmod(posX[bird], gPara.fieldLength);
+            posY[bird] = fmod(posY[bird], gPara.fieldLength);
+
+        }
+        //adjust theta
+        for(int bird=0; bird < gPara.birdNum; bird++){ //for each bird
+
+            //float meanTheta = theta[bird];
+            float sx = 0,sy = 0; 
+
+            for(int oBird=0; oBird < gPara.birdNum; oBird++){ //observe other birds, self included
+                auto distPow2 = pow(posX[bird]-posX[oBird],2) + pow(posY[bird]-posY[oBird],2);
+                if(distPow2 < pow(aPara.observeRadius,2)){ //observed
+                    sx += cos(theta[oBird]);
+                    sy += sin(theta[oBird]);
+                }
+            }
+            tempTheta[bird] = atan2(sy, sx) + (radomDist(radomGen) - 0.5) * aPara.fluctuation; //new theta
+        }
+        //copy, could be dual-buffer
+        copy(tempTheta.get(), tempTheta.get(), theta.get());
+        //memcpy(theta.get(), tempTheta.get(), gPara.birdNum * sizeof(*theta.get())); //copy to theta
+        
+    }
 }
 
 
