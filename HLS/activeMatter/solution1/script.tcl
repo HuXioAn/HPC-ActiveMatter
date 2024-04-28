81290f02f4951fa9d833cf73a903645c1cdebfc8
huxioan <hu7006@outlook.com>
2024-04-28 21:48:40 +0200

    add hls proj

---
+++ HLS/activeMatter/solution1/script.tcl
@@ -0,0 +1,15 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project activeMatter
+set_top computeActiveMatter
+open_solution "solution1"
+set_part {xc7k420tffg901-2} -tool vivado
+create_clock -period 10 -name default
+#source "./activeMatter/solution1/directives.tcl"
+#csim_design
+csynth_design
+#cosim_design
+export_design -format ip_catalog
