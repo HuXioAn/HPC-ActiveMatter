596b158b173aed731e6ab344d3361636b5a2664a
Yitong <bjlyt@foxmail.com>
2024-05-19 14:37:30 +0200

    5/19-2

--- HPC/OpenMP/activeMatterHPC_manually_asychro.cpp
+++ HPC/OpenMP/activeMatterHPC_manually_asychro.cpp
@@ -117,90 +117,93 @@ int outputToFile(ofstream& outputFile, int birdNum, arrayPtr& posX, arrayPtr& po
 
 
 void computeActiveMatter(generalPara_t gPara, activePara_t aPara, arrayPtr& posX, arrayPtr& posY, arrayPtr& theta, int threadNum){
-
     arrayPtr bufTheta(new float[gPara.birdNum]);
     arrayPtr bufposX(new float[gPara.birdNum]);
     arrayPtr bufposY(new float[gPara.birdNum]);
     ofstream outputFile;
-    float observeRadiusSqr = pow(aPara.observeRadius,2);
+    float observeRadiusSqr = pow(aPara.observeRadius, 2);
     float inscribedSquareSideLengthHalf = aPara.observeRadius / sqrt(2);
     double start_time, end_time;
     //omp_set_num_threads(threadNum);
     start_time = omp_get_wtime();
     omp_set_nested(1);
-    if(OUTPUT_TO_FILE){//save the parameter,first step to file
+    if (OUTPUT_TO_FILE) { // save the parameter, first step to file
         outputFile = ofstream(gPara.outputPath, ios::trunc);
-        if(outputFile.is_open()){
+        if (outputFile.is_open()) {
             outputFile << std::fixed << std::setprecision(3);
-        }else{
+        } else {
             cout << "[!]Unable to open output file: " << gPara.outputPath << endl;
             exit(-1);
         }
 
-        //para
-        outputFile << "generalParameter{" << "fieldLength=" << gPara.fieldLength << ",totalStep=" << gPara.totalStep << 
+        // para
+        outputFile << "generalParameter{" << "fieldLength=" << gPara.fieldLength << ",totalStep=" << gPara.totalStep <<
             ",birdNum=" << gPara.birdNum << "}" << endl;
-        //data
-        //outputToFile(outputFile, gPara.birdNum, posX, posY, theta);
+        // data
+        // outputToFile(outputFile, gPara.birdNum, posX, posY, theta);
     }
-    threadNum --;
-    //make the whole calculation part parallel
+    threadNum--;
+    // make the whole calculation part parallel
     #pragma omp parallel num_threads(2)
     {
-        for(int step=0; step < gPara.totalStep; step++){
-            #pragma omp sections{
-                #pragma omp section{
-                    #pragma omp parallel num_threads(threadNum){
+        for (int step = 0; step < gPara.totalStep; step++) {
+            #pragma omp sections
+            {
+                #pragma omp section
+                {
+                    #pragma omp parallel num_threads(threadNum)
+                    {
                         int id = omp_get_thread_num();
-                        //manually allocate threads
-                        for(int bird=id; bird < gPara.birdNum; bird+=threadNum){ //move
-                        //move
+                        // manually allocate threads
+                        for (int bird = id; bird < gPara.birdNum; bird += threadNum) { // move
+                            // move
                             bufposX[bird] = gPara.deltaTime * cos(theta[bird]) + posX[bird];
                             bufposY[bird] = gPara.deltaTime * sin(theta[bird]) + posY[bird];
 
-                        //in the field
-                            bufposX[bird] = fmod(bufposX[bird]+gPara.fieldLength, gPara.fieldLength);
-                            bufposY[bird] = fmod(bufposY[bird]+gPara.fieldLength, gPara.fieldLength);
+                            // in the field
+                            bufposX[bird] = fmod(bufposX[bird] + gPara.fieldLength, gPara.fieldLength);
+                            bufposY[bird] = fmod(bufposY[bird] + gPara.fieldLength, gPara.fieldLength);
 
                         }
-                        //use barrier to Synchronize
+                        // use barrier to Synchronize
                         #pragma omp barrier
-                        //adjust theta
-                        for(int bird=id; bird < gPara.birdNum; bird+=threadNum){ //for each bird
-                        //float meanTheta = theta[bird];
-                            float sx = 0,sy = 0; 
-                            for(int oBird=0; oBird < gPara.birdNum; oBird++){ //observe other birds, self included
+                        // adjust theta
+                        for (int bird = id; bird < gPara.birdNum; bird += threadNum) { // for each bird
+                            // float meanTheta = theta[bird];
+                            float sx = 0, sy = 0;
+                            for (int oBird = 0; oBird < gPara.birdNum; oBird++) { // observe other birds, self included
 
-                                auto xDiffAbs = abs(bufposX[bird]-bufposX[oBird]);
-                                auto yDiffAbs = abs(bufposY[bird]-bufposY[oBird]);
-                        
-                                if((xDiffAbs > aPara.observeRadius) || 
-                                    (yDiffAbs > aPara.observeRadius) 
+                                auto xDiffAbs = abs(bufposX[bird] - bufposX[oBird]);
+                                auto yDiffAbs = abs(bufposY[bird] - bufposY[oBird]);
+
+                                if ((xDiffAbs > aPara.observeRadius) ||
+                                    (yDiffAbs > aPara.observeRadius)
                                     || ((xDiffAbs > inscribedSquareSideLengthHalf) && (yDiffAbs > inscribedSquareSideLengthHalf))
-                                    )continue;//ignore birds outside the circumscribed square and 4 corners
-                                if((xDiffAbs < inscribedSquareSideLengthHalf) && 
-                                    (yDiffAbs < inscribedSquareSideLengthHalf)){ //birds inside the inscribed square
+                                    ) continue; // ignore birds outside the circumscribed square and 4 corners
+                                if ((xDiffAbs < inscribedSquareSideLengthHalf) &&
+                                    (yDiffAbs < inscribedSquareSideLengthHalf)) { // birds inside the inscribed square
                                     sx += cos(theta[oBird]);
                                     sy += sin(theta[oBird]);
-                                }else{
+                                } else {
                                     auto distPow2 = pow(xDiffAbs, 2) + pow(yDiffAbs, 2);
-                                    if(distPow2 < observeRadiusSqr){ //observed
+                                    if (distPow2 < observeRadiusSqr) { // observed
                                         sx += cos(theta[oBird]);
                                         sy += sin(theta[oBird]);
                                     }
                                 }
                             }
-                            bufTheta[bird] = atan2(sy, sx) + (randomDist(randomGen) - 0.5) * aPara.fluctuation; //new theta
+                            bufTheta[bird] = atan2(sy, sx) + (randomDist(randomGen) - 0.5) * aPara.fluctuation; // new theta
                         }
                     }
                 }
-                #pragma omp sections{
-                    if(OUTPUT_TO_FILE)
+                #pragma omp section
+                {
+                    if (OUTPUT_TO_FILE)
                         outputToFile(outputFile, gPara.birdNum, posX, posY, theta);
                 }
             }
-            #pragma omp barrier
-            if(id == 0){
+            #pragma omp master
+            {
                 auto tempPtr = theta;
                 theta = bufTheta;
                 bufTheta = tempPtr;
@@ -213,14 +216,15 @@ void computeActiveMatter(generalPara_t gPara, activePara_t aPara, arrayPtr& posX
             }
 
         }
-        
+
     }
     end_time = omp_get_wtime();
-    printf("Execution time of %d threads:%lf\n",threadNum, end_time - start_time);
-    if(OUTPUT_TO_FILE)outputFile.close();
+    printf("Execution time of %d threads:%lf\n", threadNum + 1, end_time - start_time);
+    if (OUTPUT_TO_FILE) outputFile.close();
     delete[] bufposX;
     delete[] bufposY;
     delete[] bufTheta;
 }
+
 
 
